6900d4034a89295b11f40746ccfd0512b7081d45
ShaxxKh <sop.ugc2@gmail.com>
2021-04-21 13:46:56 +0500

    feat: add courses block

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 let heroTextCarousel = $(".carousel-text");
+const coursesCarousel = $(".courses__carousel");
 
 $(document).ready(function () {
 	heroTextCarousel.owlCarousel({
@@ -7,7 +8,33 @@ $(document).ready(function () {
 		autoplay: true,
 		loop: true,
 		autoplayTimeout: 5000,
-		stageClass: "hero__carousel",
+		nav: true,
+		// stageOuterClass: "hero__carousel",
+		navContainerClass: "hero__nav-container",
+		navText: [
+			"<img class='hero__nav-img--left' src='./../images/left.svg'>",
+			"<img class='hero__nav-img--right' src='./../images/right.svg'>",
+		],
+		navClass: ["hero__nav-left", "hero__nav-right"],
+		animateOut: "animate__slideOutLeft",
+		animateIn: "animate__slideInRight",
+	});
+});
+
+$(document).ready(function () {
+	coursesCarousel.owlCarousel({
+		items: 3,
+		margin: 20,
+		autoplay: true,
+		loop: true,
+		autoplayTimeout: 5000,
+		// stageOuterClass: "hero__carousel",
+		// navContainerClass: "hero__nav-container",
+		// navText: [
+		// 	"<img class='hero__nav-img--left' src='./../images/left.svg'>",
+		// 	"<img class='hero__nav-img--right' src='./../images/right.svg'>",
+		// ],
+		// navClass: ["hero__nav-left", "hero__nav-right"],
 		animateOut: "animate__slideOutLeft",
 		animateIn: "animate__slideInRight",
 	});
